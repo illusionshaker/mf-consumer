c59948bddfd6645df936a34563d879d45979f584
Mathew Ng <mathew.ng@iress.com>
2021-06-18 17:00:30 +1000

    initial commit for consumer files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Consumer Application
+
+Webpack federation - consumes the Header component exposed in mf-header
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+import Header from "header/Header";
+
+const App = () => (
+  <div>
+    <Header />
+    <div>I'm the consumer app</div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
